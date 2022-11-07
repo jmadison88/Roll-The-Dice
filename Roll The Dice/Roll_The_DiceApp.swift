ab27a18157bc0952699e8d8248f064d28a1dddb5
jmadison <jmad99@sbcglobal.net>
2022-11-07 09:41:21 -0600

    Initial Commit

---
+++ Roll The Dice/Roll_The_DiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  Roll_The_DiceApp.swift
+//  Roll The Dice
+//
+//  Created by Josh Madison on 11/7/22.
+//
+
+import SwiftUI
+
+@main
+struct Roll_The_DiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
